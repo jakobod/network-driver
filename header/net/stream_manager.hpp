744fd2e39071bb7e605886a499a0741ea78dd485
Jakob Otto <jakob.otto@haw-hamburg.de>
2021-09-08 17:28:06 +0200

    Add scaffold of stream manager + tests

---
+++ header/net/stream_manager.hpp
@@ -0,0 +1,96 @@
+/**
+ *  @author Jakob Otto
+ *  @email jakob.otto@haw-hamburg.de
+ */
+
+#pragma once
+
+#include "net/socket_manager.hpp"
+
+#include "fwd.hpp"
+#include "net/stream_socket.hpp"
+
+namespace net {
+
+/// Manages the lifetime of a socket.
+template <class Application>
+class stream_manager : public socket_manager {
+public:
+  stream_manager(stream_socket handle, multiplexer* mpx)
+    : socket_manager(handle, mpx) {
+    // nop
+  }
+
+  // -- socket_manager API -----------------------------------------------------
+
+  bool handle_read_event() override {
+    for (int i = 0; i < 20; ++i) {
+      auto ptr = recv_buffer_.data() + received_;
+      auto size = recv_buffer_.size() - received_;
+      auto read_res = read(handle<stream_socket>(), std::span(ptr, size));
+      if (read_res == 0)
+        return false; // Disconnect
+      if (read_res < 0) {
+        if (last_socket_error_is_temporary()) {
+          return true;
+        } else {
+          mpx()->handle_error(detail::error(detail::socket_operation_failed,
+                                            "[socket_manager.read()] "
+                                              + last_socket_error_as_string()));
+          return false;
+        }
+      }
+      received_ += read_res;
+      if (recv_buffer_.size() == received_)
+        if (!application_.consume(*this, recv_buffer_))
+          return false;
+    }
+    return true;
+  }
+
+  bool handle_write_event() override {
+    for (int i = 0; i < 20; ++i) {
+      auto num_bytes = send_buffer_.size() - written_;
+      auto write_res = write(handle<stream_socket>(),
+                             std::span(send_buffer_.data(), num_bytes));
+      if (write_res > 0) {
+        send_buffer_.erase(send_buffer_.begin(),
+                           send_buffer_.begin() + write_res);
+
+        return send_buffer_.empty();
+      } else if (write_res <= 0) {
+        if (last_socket_error_is_temporary())
+          return true;
+        else
+          mpx()->handle_error(detail::error(detail::socket_operation_failed,
+                                            "[socket_manager.write()] "
+                                              + last_socket_error_as_string()));
+      }
+    }
+    return false;
+  }
+
+  std::string me() const override {
+    return "stream_manager";
+  }
+
+  // -- public API -------------------------------------------------------------
+
+  /// Configures the amount to be read next
+  void configure_next_read(size_t size) {
+    received_ = 0;
+    if (recv_buffer_.size() != size)
+      recv_buffer_.resize(size);
+  }
+
+private:
+  Application application_;
+
+  size_t received_ = 0;
+  size_t written_ = 0;
+
+  detail::byte_buffer recv_buffer_;
+  detail::byte_buffer send_buffer_;
+};
+
+} // namespace net
---
+++ test/net/stream_manager.cpp
@@ -0,0 +1,84 @@
+/**
+ *  @author Jakob Otto
+ *  @email jakob.otto@haw-hamburg.de
+ */
+
+#include <gtest/gtest.h>
+
+#include <numeric>
+
+#include "detail/error.hpp"
+#include "fwd.hpp"
+#include "net/multiplexer.hpp"
+#include "net/stream_manager.hpp"
+#include "net/stream_socket.hpp"
+
+namespace {
+
+struct dummy_multiplexer : public net::multiplexer {
+  detail::error init(net::socket_manager_factory_ptr) override {
+    return detail::none;
+  }
+
+  void start() override {
+    // nop
+  }
+
+  void shutdown() override {
+    // nop
+  }
+
+  void join() override {
+    // nop
+  }
+
+  void handle_error(detail::error err) override {
+    FAIL() << "There should be no errors! " << err << std::endl;
+  }
+
+  void add(net::socket_manager_ptr, net::operation) override {
+    // nop
+  }
+
+  void enable(net::socket_manager&, net::operation) override {
+    // nop
+  }
+
+  void disable(net::socket_manager&, net::operation, bool) override {
+    // nop
+  }
+};
+
+struct dummy_application {
+  template <class Parent>
+  bool consume(Parent&, detail::const_byte_span) {
+    return true;
+  }
+};
+
+using manager_type = net::stream_manager<dummy_application>;
+
+struct stream_manager_test : public testing::Test {
+  stream_manager_test()
+    : sockets{net::stream_socket{net::invalid_socket_id},
+              net::stream_socket{net::invalid_socket_id}} {
+    auto socket_res = net::make_stream_socket_pair();
+    EXPECT_EQ(std::get_if<detail::error>(&socket_res), nullptr);
+    sockets = std::get<net::stream_socket_pair>(socket_res);
+  }
+
+  net::stream_socket_pair sockets;
+  dummy_multiplexer mpx;
+  detail::byte_array<1024> data;
+};
+
+} // namespace
+
+TEST_F(stream_manager_test, handle_read_event) {
+  manager_type mgr(sockets.first, &mpx);
+  ASSERT_EQ(write(sockets.second, data), data.size());
+}
+
+TEST_F(stream_manager_test, handle_write_event) {
+  // nop
+}
