b6e922b158bd8c1b6fad832f6f209262ea433040
Jakob Otto <jakob.otto@haw-hamburg.de>
2022-10-19 16:03:38 +0200

    Add pretty formatting

--- libnet/util/logger.hpp
+++ libnet/util/logger.hpp
@@ -15,12 +15,13 @@
 namespace util {
 
 class logger {
-  static constexpr const std::string_view esc_seq_reset{"\033[0m"};
-  static constexpr const std::string_view esc_seq_bold{"\033[1m"};
-  static constexpr const std::string_view esc_seq_reset_bold{"\033[22m"};
-  static constexpr const std::string_view esc_seq_red{"\033[31m"};
-  static constexpr const std::string_view esc_seq_green{"\033[32m"};
-  static constexpr const std::string_view esc_seq_yellow{"\033[33m"};
+  /// Escape sequences for formatting the logging output
+  static constexpr const std::string_view reset_formatting{"\033[0m"};
+  static constexpr const std::string_view reset_bold{"\033[22m"};
+  static constexpr const std::string_view debug_formatting{"\033[1m"};
+  static constexpr const std::string_view info_formatting{"\033[1;32m"};
+  static constexpr const std::string_view warning_formatting{"\033[1;33m"};
+  static constexpr const std::string_view error_formatting{"\033[1;31m"};
 
   struct log_level {};
 
@@ -32,8 +33,8 @@ public:
 
   template <meta::derived_from<log_level> Level, class... Ts>
   static void
-  log(const std::string& file_name, const int line, const Ts&... ts) {
-    const std::string location = (file_name + ":" + std::to_string(line));
+  log(const std::string& file_name, const std::size_t line, const Ts&... ts) {
+    const std::string location = file_name + ":" + std::to_string(line);
     if constexpr (meta::same_as<Level, debug>) {
       log_debug(location, ts...);
     } else if constexpr (meta::same_as<Level, info>) {
@@ -46,36 +47,33 @@ public:
   }
 
 private:
-  template <class T>
-  static void append_to_stream(const T& t) {
-    std::cout << t;
+  template <class... Ts>
+  static void log_line(const Ts&... ts) {
+    (std::cout << ... << ts) << std::endl;
   }
 
   template <class... Ts>
   static void log_debug(const std::string& location, const Ts&... ts) {
-    std::cout << esc_seq_bold << "[Debug]   " << location << " - "
-              << esc_seq_reset_bold << (... << ts) << std::endl;
+    log_line(debug_formatting, "[Debug]   ", location, " - ", reset_bold, ts...,
+             reset_formatting);
   }
 
   template <class... Ts>
   static void log_info(const std::string& location, const Ts&... ts) {
-    std::cout << esc_seq_green << esc_seq_bold << "[Info]    " << location
-              << " - " << esc_seq_reset_bold << (... << ts) << esc_seq_reset
-              << std::endl;
+    log_line(info_formatting, "[Info]    ", location, " - ", reset_bold, ts...,
+             reset_formatting);
   }
 
   template <class... Ts>
   static void log_warning(const std::string& location, const Ts&... ts) {
-    std::cout << esc_seq_yellow << esc_seq_bold << "[Warning] " << location
-              << " - " << esc_seq_reset_bold << (... << ts) << esc_seq_reset
-              << std::endl;
+    log_line(warning_formatting, "[Warning] ", location, " - ", reset_bold,
+             ts..., reset_formatting);
   }
 
   template <class... Ts>
   static void log_error(const std::string& location, const Ts&... ts) {
-    std::cout << esc_seq_red << esc_seq_bold << "[Error]   " << location
-              << " - " << esc_seq_reset_bold << (... << ts) << esc_seq_reset
-              << std::endl;
+    log_line(error_formatting, "[Error]   ", location, " - ", reset_bold, ts...,
+             reset_formatting);
   }
 };
 
