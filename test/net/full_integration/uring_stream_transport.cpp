66dcfc594bf3bf97059464b4af1823d8f703add0
Jakob Otto <github@jakob-otto.de>
2026-04-11 00:27:02 +0200

    Add full integration test for stream_transports

---
+++ test/net/full_integration/uring_stream_transport.cpp
@@ -0,0 +1,171 @@
+/**
+ *  @author    Jakob Otto
+ *  @file      multiplexer.cpp
+ *  @copyright Copyright 2023 Jakob Otto. All rights reserved.
+ *             This file is part of the network-driver project, released under
+ *             the GNU GPL3 License.
+ */
+
+#if defined(LIB_NET_URING)
+
+#  include "net_test.hpp"
+
+#  include "net/detail/manager_base.hpp"
+#  include "net/detail/stream_transport.hpp"
+#  include "net/detail/uring_manager.hpp"
+#  include "net/detail/uring_multiplexer.hpp"
+
+#  include "net/ip/v4_address.hpp"
+#  include "net/ip/v4_endpoint.hpp"
+
+#  include "net/event_result.hpp"
+#  include "net/socket/stream_socket.hpp"
+#  include "net/socket/tcp_stream_socket.hpp"
+#  include "net/socket_guard.hpp"
+
+#  include "util/byte_literals.hpp"
+#  include "util/config.hpp"
+#  include "util/error.hpp"
+#  include "util/error_or.hpp"
+#  include "util/intrusive_ptr.hpp"
+
+#  include <chrono>
+#  include <memory>
+#  include <thread>
+#  include <tuple>
+
+using namespace net;
+using namespace net::ip;
+using namespace util::byte_literals;
+using namespace std::chrono_literals;
+
+namespace {
+
+struct mirror_application {
+  static constexpr std::size_t min_read_size = 1_KB;
+  static constexpr std::size_t max_read_size = 8_KB;
+  static constexpr std::size_t max_write_size = 8_KB;
+
+  mirror_application(detail::transport_base& parent) : parent_{parent} {
+    // nop
+  }
+
+  util::error init(const util::config&) {
+    parent_.configure_next_read(
+      receive_policy::between(min_read_size, max_read_size));
+    return util::none;
+  }
+
+  event_result produce() { return event_result::ok; }
+
+  bool has_more_data() const noexcept { return false; }
+
+  event_result consume(util::const_byte_span data) {
+    parent_.enqueue(data);
+    parent_.configure_next_read(
+      receive_policy::between(min_read_size, max_read_size));
+    return event_result::ok;
+  }
+
+  event_result handle_timeout(uint64_t) { return event_result::ok; }
+
+private:
+  detail::transport_base& parent_;
+};
+
+// -- Test fixture -------------------------------------------------------------
+
+struct uring_stream_transport_full_integration : public testing::Test {
+  using stack_type = detail::uring_stream_transport<mirror_application>;
+
+  uring_stream_transport_full_integration() {
+    auto factory
+      = [this](net::socket handle,
+               detail::uring_multiplexer* mpx) -> detail::uring_manager_ptr {
+      return util::make_intrusive<stack_type>(
+        socket_cast<stream_socket>(handle), mpx);
+    };
+    mpx = UNPACK_EXPRESSION(
+      detail::make_uring_multiplexer(std::move(factory), cfg));
+    default_num_socket_managers = mpx->num_socket_managers();
+    for (std::size_t i = 0; i < data.size(); ++i) {
+      data[i] = static_cast<std::byte>(i);
+    }
+  }
+
+  void SetUp() override {
+    mpx->start();
+    socket = connect_to_mpx();
+    const auto res = wait_for([this] {
+      return mpx->num_socket_managers() >= (default_num_socket_managers + 1);
+    });
+    ASSERT_TRUE(res);
+    ASSERT_TRUE(nonblocking(*socket, true));
+  }
+
+  void TearDown() override {
+    mpx->shutdown();
+    mpx->join();
+  }
+
+  tcp_stream_socket connect_to_mpx() {
+    auto sock = UNPACK_EXPRESSION(make_connected_tcp_stream_socket(
+      v4_endpoint{v4_address::localhost, mpx->port()}));
+    return sock;
+  }
+
+  bool
+  wait_for(std::function<bool()> predicate, std::size_t max_retries = 10,
+           std::chrono::steady_clock::duration sleep_interval = 100ms) const {
+    for (std::size_t i = 0; i < max_retries; ++i) {
+      if (predicate()) {
+        break;
+      } else {
+        std::this_thread::sleep_for(sleep_interval);
+      }
+    }
+    return predicate();
+  }
+
+  detail::uring_multiplexer_ptr mpx;
+  net::socket_guard<tcp_stream_socket> socket;
+  std::size_t default_num_socket_managers{0};
+  util::config cfg;
+  util::byte_array<10_KB> data;
+};
+
+} // namespace
+
+TEST_F(uring_stream_transport_full_integration, mirror) {
+  {
+    std::size_t written = 0;
+    for (std::size_t i = 0; (i < 10) && written < data.size(); ++i) {
+      const auto [event_res, num_bytes_written] = test::write(
+        *socket, std::span{(data.data() + written), (data.size() - written)});
+      ASSERT_NE(event_res, event_result::error);
+      if (event_res == event_result::temporary_error) {
+        std::this_thread::sleep_for(10ms);
+      }
+      written += num_bytes_written;
+    }
+    ASSERT_EQ(written, data.size());
+  }
+  util::byte_array<10_KB> receive_buffer = {};
+  {
+    std::size_t received = 0;
+    for (std::size_t i = 0; (i < 10) && (received < 10_KB); ++i) {
+      const auto [event_res, num_bytes_received]
+        = test::read(*socket, std::span{(receive_buffer.data() + received),
+                                        (receive_buffer.size() - received)});
+      ASSERT_NE(event_res, event_result::error);
+      if (event_res == event_result::temporary_error) {
+        std::this_thread::sleep_for(10ms);
+      }
+      received += num_bytes_received;
+    }
+    ASSERT_EQ(received, 10_KB);
+  }
+  EXPECT_EQ(data, receive_buffer);
+}
+
+#endif
